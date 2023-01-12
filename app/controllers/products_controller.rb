a8982feb5d607a143646f2937aab4e874f8673b2
soham baghela <sobaghela@bestpeers.com>
2023-01-12 21:45:03 +0530

    Today I worked on Updating of product can only done by same admin  adding successfully

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,11 @@
 class ProductsController < ApplicationController
   # before_action :check_user_authentication
   before_action :check_admin, only: [:new, :create]
-  
-
+  before_action :check_admin_for_update, only: [:edit, :update]
   def index
-    @products = Product.all
+    @products = []
+    @products << Product.where(user_id: params[:user_id])
+    @products = @products.flatten
     @user = params[:user_id]
     @user_detail = User.find(@user)
   end
@@ -24,10 +25,8 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     @user_id = params[:user_id]
     @product.user_id = @user_id
-    #byebug
     if @product.save
       flash[:notice] = "Your Product is Saved Successfully"
-      # byebug
       redirect_to user_product_path(user_id: @user_id, id: @product.id)
     else
       flash[:notice] = "Please try again"
@@ -35,22 +34,34 @@ class ProductsController < ApplicationController
     end
   end
 
-  def update
+  def edit
+    # byebug
+    @product = Product.find(params[:id])
   end
 
-  private 
+  def update
+    @product = Product.find(params[:id])
+    # byebug
+    if @product.update(product_params)
+      # byebug
+      flash[:notice] = "Product updated"
+      redirect_to new_user_product_path 
+    else
+      flash[:notice] = "Product has not updated"
+      redirect_to new_user_product_path 
+    end
+  end
+
+  private
   def product_params
     params.require(:product).permit!
   end
   def match_users
     (current_user.id).to_s.eql?(params[:user_id])
-
   end
   def check_admin
-    # byebug
     if match_users
       if current_user.role == "admin"
-      # byebug
       else
         redirect_to root_path
       end
@@ -58,5 +69,18 @@ class ProductsController < ApplicationController
       redirect_to root_path
     end
   end
-  
+  def match_admins
+    @id = (params[:id]).to_i
+    @product = Product.find(@id)
+    @user_id = @product.user_id
+    current_user.id.eql?(@user_id)
+  end
+  def check_admin_for_update
+      if match_admins
+        flash[:notice] = "Yeah you can Update"
+      else
+        flash[:notice] = "Sorry you can not update this Product "
+        redirect_to user_products_path
+      end
+  end
 end
